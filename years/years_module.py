952c9c2a08e9af3dbb5616dd92e854c8d9b94a0a
janos-kakuszi <jan.kakuszi@gmail.com>
2016-11-14 21:53:39 +0100

    Update years_module.py

--- years/years_module.py
+++ years/years_module.py
@@ -2,10 +2,18 @@ import datetime
 
 
 def years(age):
+    diff = 100-age
+    current = datetime.date.today()
+    return int(current.year) + diff
     return
 
 
 def main():
+    user = input("Give your name: ")
+    age = int(input("Give your age: "))
+    print("Welcome " + user)
+    years(age)
+    print(user + ", you will turn 100 in " + str(years(age)))
     return
 
 
