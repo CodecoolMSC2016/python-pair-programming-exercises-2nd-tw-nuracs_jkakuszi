86c24847c8edd61df6e8db17d459749c5e66a91a
janos-kakuszi <jan.kakuszi@gmail.com>
2016-11-14 14:15:59 +0100

    Update fizzbuzz_module.py

--- fizzbuzz/fizzbuzz_module.py
+++ fizzbuzz/fizzbuzz_module.py
@@ -1,8 +1,21 @@
 def fizzbuzz(number):
-    return
+    if number % 5 == 0 and number % 3 == 0:
+        return "FizzBuzz"
 
+    elif number % 3 == 0:
+        return "Fizz"
+
+    elif number % 5 == 0:
+        return "Buzz"
+
+    else:
+        return number
+  
 
 def main():
+    for i in range(1, 101):
+        print(fizzbuzz(i))
+        fizzbuzz(i)
     return
 
 if __name__ == '__main__':
