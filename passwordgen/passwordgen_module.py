1ea61fb2d8ca3e8152abad39ebaec6dc1404175b
janos-kakuszi <jan.kakuszi@gmail.com>
2016-11-14 22:05:53 +0100

    Update passwordgen_module.py

--- passwordgen/passwordgen_module.py
+++ passwordgen/passwordgen_module.py
@@ -1,10 +1,25 @@
+import random
+import string
+
+
 def passwordgen():
-    return
+    choose = input("Wanna strong password? (If yes enter Y) :")
+    mypw = []
+    symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?"]
+    if choose == "Y":
+        for i in range(2):
+            mypw.append(str(random.randint(1, 9)))
+            mypw.append(random.choice(string.ascii_uppercase))
+            mypw.append(random.choice(string.ascii_lowercase))
+            mypw.append(random.choice(symbols))
+        print("Your password: " + "".join(mypw))
+        mypw = str(mypw)
+    return mypv
 
 
 def main():
+    passwordgen()
     return
-
 
 if __name__ == '__main__':
     main()
