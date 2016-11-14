18b5a3be0be7651a3b2ecfe076b90e13ec57da56
janos-kakuszi <jan.kakuszi@gmail.com>
2016-11-14 22:14:03 +0100

    Update passwordgen_module.py

--- passwordgen/passwordgen_module.py
+++ passwordgen/passwordgen_module.py
@@ -14,12 +14,22 @@ def passwordgen():
             mypw.append(random.choice(symbols))
         print("Your password: " + "".join(mypw))
         mypw = str(mypw)
-    return mypv
+        return mypw
+    else:
+        easy = []
+        choose_easy = ["Kutya1234!", "Cica4321!", "Jelszo123!", "Alma2222!"]
+        easy.append(random.choice(choose_easy))
+        print("Your password: " + easy[0])
+        easy = str(easy)
+
+    return easy
 
 
 def main():
     passwordgen()
+
     return
 
+
 if __name__ == '__main__':
-    main()
+   main()
